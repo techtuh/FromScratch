c08bcc69506ce2003a2658f220891b636020ef41
techtuh <139290836+techtuh@users.noreply.github.com>
2023-08-03 21:27:21 -0400

    send by tuhin

---
+++ FromScratch/src/main/java/automationexercise/Local.java
@@ -0,0 +1,10 @@
+package automationexercise;
+
+public class Local {
+
+	public static void main(String[] args) {
+		System.out.println("Local area network call Lan");
+		System.out.println("Metropolitan Area Network cal Man");
+	}
+
+}
